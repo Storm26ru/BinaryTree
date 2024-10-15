f0f891eda5e13f325ea6f1a943087d94626085aa
Storm <Storm@STORMSSD>
2024-10-16 01:48:43 +0300

    performance start

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 using namespace std;
 #define tab "\t"
+#include<ctime>
 
 class Tree
 {
@@ -9,8 +10,8 @@ class Tree
 		int Data;
 		Element* pLeft;
 		Element* pRight;
-		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) : Data(Data), pLeft(pLeft), pRight(pRight) { cout << "EConstructor: " << this << endl; }
-		~Element() { cout << "EDestructor: " << this << endl; }
+		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) : Data(Data), pLeft(pLeft), pRight(pRight) { /*cout << "EConstructor: " << this << endl;*/ }
+		~Element() { /*cout << "EDestructor: " << this << endl;*/ }
 		friend class Tree;
 	}*Root;
 
@@ -37,6 +38,7 @@ class Tree
 			if (Root->pRight == nullptr)Root->pRight = new Element(Data);
 			else insert(Data, Root->pRight);
 		}
+		//Balance();
 	}
 	int MinValue(Element* Root)const
 	{
@@ -54,35 +56,13 @@ class Tree
 		Clear(Root->pRight);
 		delete Root;
 	} 
-	void Add(Element*& Temp, Element*& Root)
-	{
-		if (Temp == nullptr)return;
-		Add(Temp->pLeft,Root);
-		Add(Temp->pRight, Root);
-		if (Root == nullptr)
-		{
-			Root = new Element(Temp->Data); return;
-		}
-		insert(Temp->Data, Root);
-	}
+	
 	void Erase(int Data, Element*& Root)
 	{
-		if (Data == Root->Data) 
-		{
-			Element*temp = Root;
-			Root = nullptr;
-			Add(temp->pLeft, Root);
-			Add(temp->pRight, Root);
-			Clear(temp);
-			return;
-		}
-		Data < Root->Data ? Erase(Data, Root->pLeft) : Erase(Data, Root->pRight);
-	}
-	void Erase1(int Data, Element*& Root)
-	{
 		if (Root == nullptr)return;
-		Erase1(Data, Root->pLeft);
-		Erase1(Data, Root->pRight);
+		Data < Root->Data ? Erase(Data, Root->pLeft) : Erase(Data, Root->pRight);
+		//Erase(Data, Root->pLeft);
+		//Erase(Data, Root->pRight);
 		if (Data == Root->Data)
 		{
 			if (Root->pLeft == Root->pRight)
@@ -95,16 +75,15 @@ class Tree
 				if (Count(Root->pLeft) > Count(Root->pRight))
 				{
 					Root->Data = MaxValue(Root->pLeft);
-					Erase1(MaxValue(Root->pLeft), Root->pLeft);
+					Erase(MaxValue(Root->pLeft), Root->pLeft);
 				}
 				else
 				{
 					Root->Data = MinValue(Root->pRight);
-					Erase1(MinValue(Root->pRight), Root->pRight);
+					Erase(MinValue(Root->pRight), Root->pRight);
 				}
 			}
 		}
-		//Data < Root->Data ? Erase1(Data, Root->pLeft) : Erase1(Data, Root->pRight);
 	}
 	int Depht(Element* Root)const
 	{
@@ -136,31 +115,38 @@ class Tree
 
 public:
 	Element* getRoot()const { return Root; };
-	Tree() { Root = nullptr; cout << "TConstructor: " << this << endl; } 
-	~Tree() { Clear(Root);  cout << "TDestructor: " << this << endl; }
+	Tree() { Root = nullptr; /*cout << "TConstructor: " << this << endl;*/ }
+	Tree(const std::initializer_list<int>list) :Tree()
+	{
+		for (int const *i = list.begin(); i != list.end(); ++i) insert(*i, Root);
+	}
+	~Tree() { /*Clear(Root); cout << "TDestructor: " << this << endl; */}
 	//						Public Methods:
 	void insert(int Data) { insert(Data, Root); }
 	void print() { print(Root); cout << endl; }
-	int MinValue() { return MinValue(Root); }
-	int MaxValue() { return MaxValue(Root); }
-	int Count() { return Count(Root); }
-	int Sum() { return Sum(Root); }
+	int MinValue()const { return MinValue(Root); }
+	int MaxValue()const { return MaxValue(Root); }
+	int Count()const { return Count(Root); }
+	int Sum()const { return Sum(Root); }
 	double Avg()const { return (double)Sum(Root)/Count(Root); }
 	void Clear() { Clear(Root); Root = nullptr; };
 	void Erase(int Data) { Erase(Data, Root); }
-	int Depht() { return Depht(Root); }
+	int Depht()const { return Depht(Root); }
 	void Balance() { Balance(Root); }
-	void Erase1(int Data) { Erase1(Data, Root); }
-
 };
-
-
-
+template<typename T> void performance(const Tree& tree,T(Tree::*Method)()const)
+{
+	clock_t start = clock();
+	cout << (tree.*Method)() << endl;
+	clock_t end = clock();
+	cout << "Производительность - " << double(end - start) / CLOCKS_PER_SEC << endl;
+}
 
 void main()
 {
 
 	setlocale(LC_ALL, "");
+
 	int n;
 	cout << "Введите рзмер дерева: "; cin >> n;
 	Tree tree;
@@ -168,20 +154,34 @@ void main()
 	{
 		tree.insert(rand() % 100);
 	}
-	tree.print();
-	cout << "Min значение дерева: " << tree.MinValue() << endl;
-	cout << "Max значение дерева: " << tree.MaxValue() << endl;
-	cout << "Размер дерева: "<<tree.Count() << endl;
-	cout << "Сумма элементов дерева: " << tree.Sum() << endl;
-	cout << "Среднее-арифметическое элементов дерева: " << tree.Avg() << endl;
-	//tree.Clear();
 	//tree.print();
-	//tree.Erase(41);
+	//cout << "Min значение дерева: " << tree.MinValue() << endl;
+	//cout << "Max значение дерева: " << tree.MaxValue() << endl;
+	//cout << "Размер дерева: " << tree.Count() << endl;
+	//cout << "Сумма элементов дерева: " << tree.Sum() << endl;
+	//cout << "Среднее-арифметическое элементов дерева: " << tree.Avg() << endl;
+	////tree.Clear();
+	////tree.print();
+	////tree.Erase(41);
+	////tree.print();
+	//cout << "Глубина дерева: " << tree.Depht() << endl;
+	//tree.Erase1(41);
 	//tree.print();
-	cout << "Глубина дерева: " << tree.Depht() << endl;
-	tree.Erase1(41);
-	tree.print();
+	//tree.Balance();
+	//Tree tree{ 41,25,67,16,89 };
+	//tree.print();
+	/*clock_t start = clock();
 	tree.Balance();
+	clock_t end = clock();
+	cout << "Производительность - " << double(end - start) / CLOCKS_PER_SEC << endl;*/
+	performance(tree, &Tree::Depht);
+	tree.Balance();
+	performance(tree, &Tree::Depht);
+	tree.print();
+
+	
+
+
 	
 	
 
